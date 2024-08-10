578bd5303cf82a6c36e404761ddf578410d770b7
Dragos Stefan Necula <dragosstefannecula@gmail.com>
2024-08-10 10:55:44 +0100

    made it so it shows me the lines with #section on them

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,15 @@ export function activate(context: vscode.ExtensionContext) {
 	const foldingProvider = vscode.languages.registerFoldingRangeProvider({ language: 'plaintext' }, {
 		provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken) {
 			const ranges: vscode.FoldingRange[] = [];
-			ranges.push(new vscode.FoldingRange(1, 2));
+			for (let i = 0; i < document.lineCount; i++) {
+				const line = document.lineAt(i);
+
+				if (line.text.includes("#SECTION")) {
+					console.log(line)
+				}
+
+				ranges.push(new vscode.FoldingRange(1, 2));
+			}
 			return ranges;
 		}
 	});
